f8c5f136436c290cabe8b777f25e1bcd0320f2ee
Jesus G.H. <jesuspuronervio@gmail.com>
2026-05-20 01:01:55 +0200

    Steps20, format and clean char countries

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // IMPORTANTE: Cada vez que hagas un cambio en tu web, 
 // cambia este nombre (ej: pipaises-v3, pipaises-v4...)
-const CACHE_NAME = 'pipaises-v8';
+const CACHE_NAME = 'pipaises-v9';
 const urlsToCache = [
   './',
   './index.html',
